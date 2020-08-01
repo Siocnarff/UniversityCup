62eb74b82ce19abcd8c344dafa78833bfb2feec5
Gianni <gianni.volpi.sa@gmail.com>
2020-08-01 12:12:16 +0200

    Added file output

--- src/Map.java
+++ src/Map.java
@@ -4,6 +4,7 @@ import shapes.Shape;
 import shapes.Shapes;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -26,7 +27,7 @@ public class Map {
             Scanner scanner = new Scanner(new File("./inputFiles/" + filename));
             String[] data = scanner.nextLine().split(",");
             numRows = Integer.parseInt(data[0]);
-            numCols = Integer.parseInt(data[0]);
+            numCols = Integer.parseInt(data[1]);
             data = scanner.nextLine().split(",");
             numUniqueShapes = Integer.parseInt(data[0]);
             data = scanner.nextLine().split(",");
@@ -49,6 +50,7 @@ public class Map {
             String[] BlockedData = scanner.nextLine().split("\\|");
             for (int i = 0; i < numBlockedCells; i++) {
                 String[] temp = BlockedData[i].split(",");
+                //System.out.println("Coords: " + Integer.parseInt(temp[0]) + " " + Integer.parseInt(temp[1]));
                 map[Integer.parseInt(temp[0])][Integer.parseInt(temp[1])] = -1;
             }
         } catch (Exception e) {
@@ -69,18 +71,19 @@ public class Map {
                 }
                 for(int k = 0; k < shapeID.length; k++) {
                     if (available[k] != 0) {
-                        insertOptimally(shapeID[k], i, j);
+                        insertOptimally(k, shapeID[k], i, j);
                     }
                 }
             }
         }
     }
 
-    private void insertOptimally(int id, int i, int j) {
+    private void insertOptimally(int index, int id, int i, int j) {
         Shape s = getShape(id);
         for (Orientation orientation : s.orientations) {
             for (int[] coors : orientation.cells) {
                 if (fits(orientation, i, j, coors)) {
+                    available[index]--;
                     insertIntoMap(s.shape_id, orientation, i, j, coors);
                 }
             }
@@ -88,8 +91,28 @@ public class Map {
     }
 
     private void insertIntoMap(int shape_id, Orientation orientation, int i, int j, int[] start) {
+        writeToFile(shape_id + "|");
+        //System.out.print(shape_id + "|");
+        String out = "";
         for (int[] coors : orientation.cells) {
             map[i + coors[0] - start[0]][j + coors[1] - start[1]] = shape_id;
+            out += (i + coors[0] - start[0]) + "," + (j + coors[1] - start[1]) + "|";
+        }
+        out = out.substring(0, out.length()-1);
+        //System.out.println(out);
+        writeToFile(out + "\n");
+    }
+
+    private void writeToFile(String data){
+        try {
+            File file = new File("out3.txt");
+            FileWriter myWriter = new FileWriter(file, true);
+            myWriter.write(data);
+            myWriter.close();
+            //System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            //System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 
@@ -107,7 +130,6 @@ public class Map {
         return true;
     }
 
-
     private Shape getShape(int id) {
         for (Shape s : shapes.shapes) {
             if (s.shape_id == id) {
@@ -115,5 +137,14 @@ public class Map {
             }
         }
         return null;
+    }
+
+    public void print(){
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                System.out.print(map[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
